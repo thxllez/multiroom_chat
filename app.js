976ac48285d2f54d8b6c869fffc0a873027c34c3
Thalles Trancoso <thallestrancoso@outlook.com>
2018-02-24 17:56:58 -0300

    Finalização do projeto.

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(3000, function(){
+var server = app.listen(3000, function(){
 	console.log('SERVER ON');
+});
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io); //cria uma espécie de "variável global" que será passada para todos os arquivos dentro da variável application.
+
+/* criar a conexão por websocket */
+io.on('connection', function(socket){
+	console.log('Usuario conectou');
+
+	socket.on('disconnect', function(){
+		console.log('Usuário desconectou');
+	});
+
+	socket.on('msgParaServidor', function(data){
+		/* DIALOGOS */
+		socket.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem});
+		socket.broadcast.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem});
+
+		/* PARTICIPANTES */
+		if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+			socket.emit('participantesParaCliente', {apelido: data.apelido});
+			socket.broadcast.emit('participantesParaCliente', {apelido: data.apelido});
+		}
+	});
 });
--- app/routes/chat.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(application){
-	application.post('/chat', function(req,res){
-		res.render("chat");
-	});
-}
